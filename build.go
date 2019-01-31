a5ec9b7dabf0b4ef17217a6857819e7cb9f6c506
Chad Retz <chad.retz@gmail.com>
2019-01-31 12:48:43 -0600

    Windows CI (#19)
    Fixes #18

--- build.go
+++ build.go
@@ -79,9 +79,9 @@ func validateEnvironment() error {
 	case "windows":
 		// Confirm it is MinGW 64
 		if byts, err := exec.Command("uname", "-a").CombinedOutput(); err != nil {
-			return fmt.Errorf("This has to be run in a MSYS or MinGW shell")
-		} else if !bytes.HasPrefix(byts, []byte("MINGW64")) {
-			return fmt.Errorf("This has to be run in a MSYS or MinGW64 shell")
+			return fmt.Errorf("This has to be run in a MSYS or MinGW shell, uname failed: %v", err)
+		} else if !bytes.HasPrefix(byts, []byte("MINGW64")) && !bytes.HasPrefix(byts, []byte("MSYS2")) {
+			return fmt.Errorf("This has to be run in a MSYS or MinGW64 shell, uname output: %v", string(byts))
 		}
 	case "linux":
 		// Make sure it's not MinGW
