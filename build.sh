b5a09af2739d391e4f36af913d872f1bc2978b09
petri <petri@localhost>
2020-08-26 08:15:30 +0200

    alpine image

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-docker-compose build
-docker-compose run torstaticbuilder /usr/local/go/bin/go run build.go build-all 
-docker-compose run torstaticbuilder strip -s /build/tor-static/tor/dist/bin/tor
-docker-compose run torstaticbuilder upx -9 /build/tor-static/tor/dist/bin/tor
-docker-compose down --rmi all
-
